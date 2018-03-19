70853d0388ea002c13b0a12237000e3e9992a4e8
andrew lupton and eric wolfe <andrew@>
2018-03-19 11:54:06 -0700

    add additional equations

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
 var add = function(firstInput, secondInput) {
   return num1 +  num2;
 };
-var num1 = prompt("Enter a number:");
-var num2 = prompt("Enter a number:");
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter a number:"));
 
 alert(add(num1, num2));
+
+
+var subtract = function(firstInput, secondInput) {
+  return num1 - num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter a number:"));
+
+alert(subtract(num1, num2));
+
+
+var multiply = function(firstInput, secondInput) {
+  return num1 * num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter a number:"));
+
+alert(multiply(num1, num2));
+
+
+
+var divide = function(firstInput, secondInput) {
+  return num1 / num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter a number:"));
+
+alert(divide(num1, num2));
