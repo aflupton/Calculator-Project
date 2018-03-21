bdd55961e831d9ccd0c66d36b4333b40c91b15da
andrew lupton and verna santos <andrew@>
2018-03-21 11:29:27 -0700

    jQuery forms and textual responses

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,29 @@
-var add = function(firstInput, secondInput) {
-  return num1 +  num2;
-};
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter a number:"));
-
-alert(add(num1, num2));
 
 
-var subtract = function(firstInput, secondInput) {
-  return num1 - num2;
-};
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter a number:"));
+    // below this line is back-end logic //
+    var add = function(add1, add2) {
+      return add1 +  add2;
+    };
+    var subtract = function(add1, add2) {
+      return add1 - add2;
+    };
+    var multiply = function(add1, add2) {
+      return add1 * add2;
+    };
+    var divide = function(add1, add2) {
+      return add1 / add2;
+    };
 
-alert(subtract(num1, num2));
+    // below this line is front-end logic //
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+
+    var num1 = parseInt($("#add1").val());
+    var num2 = parseInt($("#add2").val());
+    var result = add(num1, num2);
+    $("#output").text(result);
 
 
-var multiply = function(firstInput, secondInput) {
-  return num1 * num2;
-};
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter a number:"));
-
-alert(multiply(num1, num2));
-
-
-
-var divide = function(firstInput, secondInput) {
-  return num1 / num2;
-};
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter a number:"));
-
-alert(divide(num1, num2));
+  });
+});
